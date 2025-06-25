a44842d0694b15e1cd2943ada39d1bdfd61fc262
Anjali7020 <anjalibankar76@gmail.com>
2025-06-25 20:05:42 +0530

    Added substract features

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -2,13 +2,14 @@ class Calculator{
 public int add(int a, int b){
     return a+b;
 }
+public int sub(int a, int b){
+    return a-b;
 }
-
-
-
+}
 public class CalculatorTest{
     public static void main(String[] args){
     Calculator calc = new Calculator();
    System.out.println("Result:"+ calc.add(2, 3));
-    }
+   System.out.println("Result:"+calc.sub(3, 4));
+}
 }
